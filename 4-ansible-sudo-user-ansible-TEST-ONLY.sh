96451b52564574d330fd3f7b6b2c25eafacf99e9
Ali Akrour <ali.akrour@gmail.com>
2020-07-27 19:21:09 +0200

    pass auto dans le test de connexion Ansible suite au bootstrap

--- 4-ansible-sudo-user-ansible-TEST-ONLY.sh
+++ 4-ansible-sudo-user-ansible-TEST-ONLY.sh
@@ -1,2 +1,2 @@
-ansible -i inventaire.ini -m user -a 'name=user-ansible groups=sudo append=yes ' --user user-ansible --ask-pass --become --ask-become-pass all
+ansible -i inventaire.ini -m user -a 'name=user-ansible groups=sudo append=yes ' --user user-ansible --become --extra-vars "ansible_ssh_pass=alinuxien ansible_become_pass=alinuxien"  all
 
