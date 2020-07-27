c03318141a2efab6a45b5e0d8d625342ed1f607c
Ali Akrour <ali.akrour@gmail.com>
2020-07-27 19:27:44 +0200

    pass auto pour copie Ansible de clé ssh

--- 6-ansible-cp-ssh-key.sh
+++ 6-ansible-cp-ssh-key.sh
@@ -1,2 +1,2 @@
-ansible -i inventaire.ini -m authorized_key -a 'user=user-ansible state=present key="{{ lookup("file", "/vagrant/.ssh/id_ecdsa.pub") }}"' --user user-ansible --ask-pass --become --ask-become-pass all
+ansible -i inventaire.ini -m authorized_key -a 'user=user-ansible state=present key="{{ lookup("file", "/vagrant/.ssh/id_ecdsa.pub") }}"' --user user-ansible --become --extra-vars "ansible_ssh_pass=alinuxien ansible_become_pass=alinuxien" all
 
