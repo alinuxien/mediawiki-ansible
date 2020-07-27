b48dbe4334f86f01f654cae6d2a850df26b37cd8
Ali Akrour <ali.akrour@gmail.com>
2020-07-27 19:13:48 +0200

    pass auto dans le bootstrap Ansible

--- 3-ansible-bootstrap.sh
+++ 3-ansible-bootstrap.sh
@@ -1,3 +1,3 @@
-ansible -i inventaire.ini -m user -a 'name=user-ansible password=$6$secretsalt$iccwgsnqtsfbC2fJqVuHt5FPZKi3FX2ryqttcnscv5NnkvOaFMSdfoRH4QlihXQS4XTW3WSh1WyMvh0ROerBS/' --user root --ask-pass all
-ansible -i inventaire.ini -m user -a 'name=user-ansible groups=sudo append=yes ' --user root --ask-pass all
+ansible -i inventaire.ini -m user -a 'name=user-ansible password=$6$secretsalt$iccwgsnqtsfbC2fJqVuHt5FPZKi3FX2ryqttcnscv5NnkvOaFMSdfoRH4QlihXQS4XTW3WSh1WyMvh0ROerBS/' --user root --extra-vars "ansible_ssh_pass=alinuxien" all
+ansible -i inventaire.ini -m user -a 'name=user-ansible groups=sudo append=yes ' --user root --extra-vars "ansible_ssh_pass=alinuxien" all
 
