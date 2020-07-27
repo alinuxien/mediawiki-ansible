f4bb21112d55b64138e980988b5ab015a6e6bf29
Ali Akrour <ali.akrour@gmail.com>
2020-07-27 18:44:21 +0200

    IP fixes pour les 2 containers

--- 2-rundockernodes.sh
+++ 2-rundockernodes.sh
@@ -1,2 +1,2 @@
-docker run -d --restart always -p 32768:22 -p 8000:80 --name http1 nodes
-docker run -d --restart always -p 32769:22 -p 3306:3306 --name bdd1 nodes
+docker run -d --restart always --ip 172.17.0.2 -p 32768:22 -p 8000:80 --name http1 nodes
+docker run -d --restart always --ip 172.17.0.3 -p 32769:22 -p 3306:3306 --name bdd1 nodes
