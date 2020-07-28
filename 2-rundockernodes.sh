fe367d39fd14822f4b9d0331954cf6e75020645a
Ali Akrour <ali.akrour@gmail.com>
2020-07-28 13:05:34 +0200

    les containers se connaissent par leur hostname

--- 2-rundockernodes.sh
+++ 2-rundockernodes.sh
@@ -1,2 +1,2 @@
-docker run -d --restart always --ip 172.17.0.2 -p 32768:22 -p 8000:80 --name http1 nodes
-docker run -d --restart always --ip 172.17.0.3 -p 32769:22 -p 3306:3306 --name bdd1 nodes
+docker run -d --restart always --ip 172.17.0.2 --hostname http1 -p 32768:22 -p 8000:80 --name http1 --add-host bdd1:172.17.0.3 nodes
+docker run -d --restart always --ip 172.17.0.3 --hostname bdd1 -p 32769:22 -p 3306:3306 --name bdd1 --add-host http1:172.17.0.2 nodes
